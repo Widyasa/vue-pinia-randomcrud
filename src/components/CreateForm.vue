100c059e8947c4d8a55b68647d0c6357a634495c
widya Yasa <widyarusmananda15@gmail.com>
2024-01-30 00:03:29 +0800

    create data success

---
+++ src/components/CreateForm.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+  import {reactive} from "vue";
+  import {Subject, useSubjectStore} from "@/stores/Subject";
+
+  const {createSubject} = useSubjectStore()
+
+  const emit = defineEmits(['list-subject'])
+
+  let subjectState = reactive({
+    name: ""
+  } as Subject)
+
+  const createSubjectModel = async () => {
+    try{
+      const response = await createSubject(subjectState);
+      const {data, message} = response.data
+      if (data.statusCode === '201') {
+        emit('list-subject')
+      }
+      console.log(message)
+    } catch (error) {
+      console.error(error.message)
+    }
+  }
+  console.log()
+</script>
+
+<template>
+  <div class="register-form">
+    <h2>Register</h2>
+    <form @submit.prevent="createSubjectModel">
+      <input v-model="subjectState.name" placeholder="Name" />
+      <button
+          type="submit"
+      >
+        Save
+      </button>
+      <button type="button" @click="$emit('list-subject')">Back</button>
+    </form>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
